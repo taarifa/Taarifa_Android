b7a7397a24196855e4374edae643b85c4482be1e
eyedol <addhen@linuxmail.org>
2011-06-20 19:03:28 +0300

    Theming

--- src/com/ushahidi/android/app/data/HandleXml.java
+++ src/com/ushahidi/android/app/data/HandleXml.java
@@ -20,11 +20,21 @@
 
 package com.ushahidi.android.app.data;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import android.util.Log;
@@ -33,18 +43,6 @@ import com.ushahidi.android.app.ImageManager;
 import com.ushahidi.android.app.UshahidiApplication;
 import com.ushahidi.android.app.UshahidiPref;
 import com.ushahidi.android.app.UshahidiService;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.FactoryConfigurationError;
-import javax.xml.parsers.ParserConfigurationException;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HandleXml {
 

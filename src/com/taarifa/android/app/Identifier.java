9d47fff24e4173c247e3ec524b932509f7e0af86
Nico Weinert <nico.weinert@googlemail.com>
2011-12-24 14:14:19 +0100

    Added class to retrieve a unique id from the phone
    Code copied from
    http://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id

---
+++ src/com/taarifa/android/app/Identifier.java
@@ -0,0 +1,28 @@
+package com.taarifa.android.app;
+
+import java.util.UUID;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+
+public class Identifier {
+	private static final String CLASS_TAG = Identifier.class.getCanonicalName();
+	
+	public static String getDeviceIdentifier(Context context, ContentResolver contentResolver) {
+		final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        final String tmDevice, tmSerial, androidId;
+        tmDevice = "" + tm.getDeviceId();
+        tmSerial = "" + tm.getSimSerialNumber();
+        androidId = "" + android.provider.Settings.Secure.getString(contentResolver, android.provider.Settings.Secure.ANDROID_ID);
+
+        UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+        String deviceId = deviceUuid.toString();
+        Log.d(CLASS_TAG, "Device Id is calculated as: " + deviceId);
+        return deviceId;
+	}
+
+}
